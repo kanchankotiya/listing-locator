63d2bf837961b87e89e05a36cbe323f0690ad989
ravi4rails <ravionrails@gmail.com>
2018-02-28 18:37:48 +0530

    add validation for listing_type and tourist_place and user model

--- app/models/listing_type.rb
+++ app/models/listing_type.rb
@@ -1,6 +1,7 @@
 class ListingType < ApplicationRecord
+ validates :name, :icon, presence: true
+
  has_many :listings, dependent: :destroy 
+ 
  mount_uploader :icon, AvatarUploader
- validates :name, presence: true
- validates :icon, presence: true
 end
--- app/models/tourist_place.rb
+++ app/models/tourist_place.rb
@@ -1,24 +1,18 @@
 class TouristPlace < ApplicationRecord
+
+  validates :name, :city, :state, :country, :banner_image, :about_palce, :zip_code, presence: true 
   mount_uploader :banner_image, AvatarUploader
   has_many :images, as: :imageable
   mount_uploader :img_name, AvatarUploader
   belongs_to :touristplace_category, optional: true
   geocoded_by :address
   after_validation :geocode
-  #validations.....
-  validates :name, presence: true
-  validates :city, presence: true 
-  validates :state, presence: true 
-  validates :country, presence: true
-  validates :banner_image, presence: true 
-  validates :about_palce, presence: true 
-  validates :longitude, presence: true  
-  validates :latitude, presence: true
-  validates :zip_code, presence: true 
+   
   
-    def address
-     "#{self.city} #{self.state} #{self.zip_code} "
-    end
+  def address
+    "#{self.city} #{self.state} #{self.zip_code} "
+  end
+
 end
 
 
